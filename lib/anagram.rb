1bb4271143d2c9a43e5b197869b57021b61214ce
Rwanda S <rwanda.ace@gmail.com>
2020-04-06 15:58:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word, :list
+  
+  def initialize(word)
+    @word = word.split("").sort
+    
+  end
+  
+  def match(list)
+    return_array = []
+    @list = list
+    @list.each {|a_word| 
+    if a_word.split("").sort == @word
+      return_array << a_word
+    end
+      }
+      return_array
+  end
+  
+end
