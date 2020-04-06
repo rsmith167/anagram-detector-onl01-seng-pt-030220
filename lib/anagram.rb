030839713d750456b9c52f8f7afe1edf950bda59
Rwanda S <rwanda.ace@gmail.com>
2020-04-06 15:35:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,16 +4,14 @@ class Anagram
   attr_accessor :word, :list
   
   def initialize(word)
-    @word = []
-    @word << word
+    @word = word
   end
   
   def match(list)
     return_array = []
-    @list = []
     @list = list
     @list.each {|a_word| 
-    if a_word.sort == @word.sort
+    if a_word.split("").sort == @word.sort
       return_array << a_word
     end
       }
